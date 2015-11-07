ed5700a83b86966438fc6dd418d2a10f51e3613f
lijiancheng <lijiancheng0614@gmail.com>
2015-11-07 21:56:14 +0800

    增加 递增进位制数法、递减进位制数法。

---
+++ project1/permutation_incre.cpp
@@ -0,0 +1,108 @@
+/**
+* Author: Jiancheng Li
+* Description: generate all permutations of n using 递增进位制数法.
+*/
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef vector<int> vi;
+struct IncreMediaNumber
+{
+    vi a; // n - 1 elements, n >= 1
+    int n;
+    IncreMediaNumber(vi p)
+    {
+        n = p.size();
+        // find the i_th smallest element.
+        int rank[n];
+        for (int i = 0; i < n; ++i)
+            rank[p[i]] = i;
+        // assume the element in a is 0 to n - 1.
+        for (int i = n - 1; i; --i)
+        {
+            int k = rank[i];
+            int s = 0;
+            for (int j = k + 1; j < n; ++j)
+                s += p[j] < p[k];
+            a.push_back(s);
+        }
+    }
+    vi getPermutation()
+    {
+        vi ans(n, 0);
+        for (int i = 0; i < n - 1; ++i)
+        {
+            int s = n;
+            for (int j = 0; j <= a[i]; ++j)
+            {
+                --s;
+                while (ans[s])
+                    --s;
+            }
+            ans[s] = n - 1 - i;
+        }
+        return ans;
+    }
+    IncreMediaNumber& operator+=(const int d)
+    {
+        if (n < 2)
+            return *this;
+        a[n - 2] += d;
+        for (int i = n - 2; i > 0 && a[i] >= n - i; --i)
+        {
+            a[i - 1] += a[i] / (n - i);
+            a[i] %= (n - i);
+        }
+        // assume no overflow
+        return *this;
+    }
+    void print()
+    {
+        printf("%d", a[0]);
+        for (int i = 1; i < n - 1; ++i)
+            printf(" %d", a[i]);
+        puts("");
+    }
+};
+
+void output(vi& a)
+{
+    // print the permutation
+    int n = a.size();
+    printf("%d", a[0] + 1);
+    for (int i = 1; i < n; ++i)
+        printf(" %d", a[i] + 1);
+    puts("");
+}
+
+void solve(int n)
+{
+    vi a;
+    int count = 1;
+    for (int i = 1; i <= n; ++i)
+    {
+        a.push_back(i - 1);
+        count *= i;
+    }
+    while (count--)
+    {
+        output(a);
+        if (count)
+        {
+            IncreMediaNumber m(a);
+            m += 1;
+            a = m.getPermutation();
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    freopen("output.txt", "w", stdout);
+    scanf("%d", &n);
+    solve(n);
+    return 0;
+}
